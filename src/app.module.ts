517c18f7cd61531d0ee7a9ed200cb56623f558b8
donggi22 <muse1956@naver.com>
2026-08-30 13:47:47 +0900

    프로젝트 정리 및 chat API 리소스 생성

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,15 @@
 import { Module, Logger } from '@nestjs/common';
 import { createObserveModule } from '@nestjs/observe';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ConfigModule } from './config/config.module';
 import { pinoHttp } from './config/pino.config';
 import { LoggerModule } from 'nestjs-pino';
 import { globalProviders } from './core/providers';
+import { ChatModule } from './modules/chat/chat.module';
 
 export const { ObserveModule, ObserveInstrument } = createObserveModule();
 
 @Module({
-  imports: [LoggerModule.forRoot({ pinoHttp }), ConfigModule],
-  controllers: [AppController],
-  providers: [AppService, ...globalProviders],
+  imports: [LoggerModule.forRoot({ pinoHttp }), ConfigModule, ChatModule],
+  providers: [...globalProviders],
 })
 export class AppModule {}
